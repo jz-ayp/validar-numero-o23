b57a1a9968ea4c73a4b4904a0208dbfee7956678
jzaldivar <jorge.zaldivar@gmail.com>
2023-10-30 22:23:39 -0600

    Remove solution

--- validar_numero.py
+++ validar_numero.py
@@ -1,40 +1,6 @@
-# Ejercicio examen parcial 2
-# Algoritmos y Programación
-# Otoño 2023
-
 def validar_numero(cadena):
-    "Verifica si la cadena es un número real válido"
-    puntos = 0
-    signos = 0
-    error = False
-    for posicion, caracter in enumerate(cadena):
-        # Sólo dígitos, puntos o signos + o -
-        if not caracter.isdigit():
-            # Sólo un punto
-            if caracter == ".":
-                puntos += 1
-                if puntos > 1:
-                    error = True
-                    break
-            # Solo un signo
-            elif caracter in "+-":
-                signos += 1
-                if signos > 1:
-                    error = True
-                    break
-                # Y tiene que ir al principio o al final
-                if posicion != 0 and posicion != len(cadena) - 1:
-                    error = True
-                    break
-            else:
-                # Ningún otro carácter es válido
-                error = True
-    return not error
+    # Borra la instrucción pass y escribe aquí tu código
+    pass
 
 
-# Probar función
-cadena = input("Introduzca cadena a probar: ")
-if validar_numero(cadena):
-    print("La cadena sí cumple los requisitos.")
-else:
-    print("La cadena no cumple los requisitos.")
+# Escribe aquí código para probar la función
